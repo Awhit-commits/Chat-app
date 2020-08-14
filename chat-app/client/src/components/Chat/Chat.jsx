cb497f30678b194626c68b073f0fcb595f3ebb7a
Awhit-commits <ndrw_whtmr@ymail.com>
2020-08-14 18:20:51 -0500

    Fixing connection between client and server

--- chat-app/client/src/components/Chat/Chat.jsx
+++ chat-app/client/src/components/Chat/Chat.jsx
@@ -128,7 +128,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = window.location.hostname;
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
